5f9ea6de22ab5c399f4f067b3a4a874bce794f14
Tom Meiselbach <tom.meiselbach@addvalue.de>
2019-07-11 17:44:42 +0200

    Elixir Version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ImagexUrlHelper.MixProject do
     [
       app: :imagex_url_helper,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

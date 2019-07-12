a0e4354f0d23a23e268de3c7c7de102b4ceb3d0a
Tom Meiselbach <tom.meiselbach@addvalue.de>
2019-07-12 09:02:59 +0200

    Options

--- lib/imagex_url_helper.ex
+++ lib/imagex_url_helper.ex
@@ -17,26 +17,33 @@ defmodule ImagexUrlHelper do
 
   ## Examples
 
-      iex> ImagexUrlHelper.build_url("http://files.example.com/image.jpg", %{resize: 0, width: 300, height: 400})
+      iex> ImagexUrlHelper.build_url("http://files.example.com/image.jpg", resize: 0, width: 300, height: 400)
       "http://img.example.com/KkoVXq-WlnBU4-KAERxGZ887..."
 
   """
-  def build_url(img_url, opts) do
+  def build_url(img_url, opts \\ []) do
     path = build_path(img_url, opts)
     signature = gen_signature(path)
 
     Path.join([prefix(), signature, path])
   end
 
-  defp build_path(img_url, opts) do
+  defp build_path(img_url, opts \\ []) do
+    resize_type = Keyword.get(opts, :resize_type, "fit")
+    width = Keyword.get(opts, :width, 200)
+    height = Keyword.get(opts, :height, 100)
+    gravity = Keyword.get(opts, :gravity, "no")
+    enlarge = Keyword.get(opts, :enlarge, 1)
+    extension = Keyword.get(opts, :extension, "jpg")
+
     Path.join([
       "/",
-      opts.resize,
-      to_string(opts.width),
-      to_string(opts.height),
-      opts.gravity,
-      to_string(opts.enlarge),
-      Base.url_encode64(img_url, padding: false) <> "." <> opts.extension
+      resize_type,
+      to_string(width),
+      to_string(height),
+      gravity,
+      to_string(enlarge),
+      Base.url_encode64(img_url, padding: false) <> "." <> extension
     ])
   end
 

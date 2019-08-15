a7f981e93e28f09371f417a3cc53e0185d99c0b2
Tom Meiselbach <tom.meiselbach@addvalue.de>
2019-08-15 15:01:12 +0200

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ImagexUrlHelper.MixProject do
   def project do
     [
       app: :imagex_url_helper,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
